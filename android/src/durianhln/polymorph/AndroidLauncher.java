3ccd127c6f1ebc629a9617cb2a0c4f5d6b411727
Darian Nguyen <dariann6@gmail.com>
2017-02-19 22:11:30 -0800

    Generated libgdx w/ Gradle

---
+++ android/src/durianhln/polymorph/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package durianhln.polymorph;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import durianhln.polymorph.Polymorph;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Polymorph(), config);
+	}
+}
