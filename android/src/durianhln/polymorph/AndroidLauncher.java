50bf4b5fec54630bcfa1f8c8d1b9d3080ccc4c10
Evan Ho <evanho9@live.com>
2018-10-02 16:16:33 -0700

    Change Android to immersive mode

--- android/src/durianhln/polymorph/AndroidLauncher.java
+++ android/src/durianhln/polymorph/AndroidLauncher.java
@@ -12,6 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
         initialize(new Polymorph(), config);
     }
 }
